6e7f7737d6e649d1acde79701d3afa80b2f5170d
PremCoder <kasturawwashunnamani@gmail.com>
2026-03-29 23:18:49 +0530

    update 3.2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const searchInput = document.getElementById("search");
 const searchButton = document.getElementById("seacrh-button");
+const categorySection = document.querySelector(".categorysection");
+
+
 searchInput.addEventListener("click", (e) => {
     e.stopPropagation();
     searchButton.style.border = "1px solid white";
-})
+});
 
 document.addEventListener("click", () => {
     searchButton.style.border = "0.1px solid #858484";
-})
+});
 
-console.log("Hello World")
+categorySection.addEventListener("wheel", (e) => {
+    if (e.deltaY !== 0) {
+        e.preventDefault();
+        categorySection.scrollLeft += e.deltaY;
+    }
+});
+
+console.log("Hello World");
